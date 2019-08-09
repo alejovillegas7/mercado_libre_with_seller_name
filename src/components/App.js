04207574a91a3c7edcd85dc63e894a2bb891b4e5
alejovillegas7 <alejovillegaslopez@gmail.com>
2019-08-08 21:15:29 -0500

    intento agregar seller

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 import SearchBar from './SearchBar'
 import ProductsList from './productsList';
 
-class App extends React.Component{
+class App extends React.PureComponent{
     
-    state={products:[]};
+    state={products:[], id:[], comments:[], id_seller:[],seller_name:[] };
     onSearchSubmit=async(term)=>{//este metodo nos ayudara a recibir el term del searchBar para hacer las peticiones al API
     
         const response=await axios.get('https://api.mercadolibre.com/sites/MLC/search',{
@@ -15,8 +15,30 @@ class App extends React.Component{
             }
         });
         
-        this.setState({products:response.data.results}); 
+        this.setState({products:response.data.results, id:this.state.products.map(producto=>producto.id),id_seller:this.state.products.map(product=>product.seller.id)});
+        this.setState({id:this.state.products.map(producto=>producto.id)}); 
+        this.setState({id_seller:this.state.products.map(product=>product.seller.id)});
         console.log(this.state.products);
+        console.log(this.state.id);
+        console.log(this.state.id_seller);
+
+        //for(var i=0;i<this.state.id.length;i++){
+            //var ITEM_ID = this.state.id[i];
+        //var response1=await axios.get(`https://api.mercadolibre.com/items/MLC456679163/description`);
+        //this.setState({comments:[...this.state.comments, response1]});
+        //}
+        
+
+        for(var i=0;i<this.state.id_seller.length;i++){
+            var SELLER_ID = this.state.id_seller[i];
+            var response2=await axios.get(`https://api.mercadolibre.com/users/${SELLER_ID}`);
+            this.setState({seller_name:[...this.state.seller_name, response2]});
+            }
+
+
+        console.log(this.state.comments);
+        console.log(this.state.seller_name);
+        
 
     }
 
@@ -25,7 +47,7 @@ class App extends React.Component{
             <div className="ui container" style={{marginTop:'10px'}}>
                 <h2><font color="#1A1E93">Alejandro Villegas Lopez - mercado libre</font></h2>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
-                <ProductsList products={this.state.products}/>
+                <ProductsList products={this.state.products} nombre_seller={this.state.seller_name}/>
             </div>
             );
     }
--- src/components/productsList.js
+++ src/components/productsList.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import './productsList.css';
+import SellerName from './SellerName';
 
 const productsList=(props)=>{
+
+    const nombre_seller = props.nombre_seller;
+
+    const nombres=[];
+
     const products=props.products.map((product)=>{
+
+        props.nombre_seller.forEach(data => {
+
+            if(product.seller.id===data.data.id){
+
+                nombres.push(data.data.nickname);
+
+            }
+            
+        });
 
         return(
 
-            <div key={product.id}>
+              <div key={product.id}>
                 <img alt={product.tags[1]} src={product.thumbnail} />
                 <ul className="ul">
                     <li><h3>{product.title}</h3></li>
                     <li><h4>Precio: {product.price} COP</h4></li>
-                    <li><h4>Descripción: {product.tags[0]}</h4></li>
+                    <SellerName nombre_seller={nombre_seller} id={product.seller.id} />
+                    <li><h4>Seller id: {product.seller.id}</h4></li>
                     <li><h4>Cantidades disponibles: {product.available_quantity}</h4></li>
                 </ul>
                 
@@ -22,7 +39,11 @@ const productsList=(props)=>{
 
     });
 
-    return <div className="products-list">{products}</div>;
+    return (
+        <div className="products-list">
+            {products}
+        </div>
+    );
 
 };
 
